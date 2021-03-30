ecb855674851b1d9f69fa037bba77daa03ff5285
Tadhg <tadhgsmboyle@gmail.com>
2021-03-30 15:30:53 -0700

    :memo: Add JS function description messages

---
+++ js/app.js
@@ -0,0 +1,45 @@
+/**
+ * Toggle the application's colour theme.
+ * Can be either light or dark.
+ */
+function toggleTheme() {
+
+}
+
+/**
+ * Toggle visibility of note textbox and title.
+ * Usage:
+ *     - "New Note" - toggle note area on
+ *     - "Save" + "Cancel" - both will toggle area off
+ */
+function toggleNoteArea() {
+
+}
+
+/**
+ * Create a new note.
+ * 
+ * @param {String} title Unique title for this note
+ * @param {String} content Content to be saved
+ */
+function saveNote(title, content) {
+
+}
+
+/**
+ * List all note titles in an HTML list.
+ * This will directly output an HTML string which can be used in an elements `innerHtml`
+ */
+function listNotes() {
+
+}
+
+/**
+ * Get content for the note stored with the supplied title when a note is clicked on side list.
+ * Will return `undefined` if note does not exist.
+ * 
+ * @param {String} title Title of note to find
+ */
+function getNoteByTitle(title) {
+
+}
